f620a7c69e7a99abcac990d1903b7d7fc0c09f04
Jordan Allen <doydle@gmail.com>
2016-07-19 12:12:25 -0600

    1

--- public/grab.js
+++ public/grab.js
@@ -16,17 +16,17 @@
 	var clicks = (function() {
 
 		var onClick = function(event) {
-			var clickObject = {};
+			var clickObject = event;
 			clickObject.currentState = document.getElementsByTagName('ui-view')[0].baseURI;
 			clickObject.__quix_site_id = window.__quix_site_id;
 			clickObject.targetElementId = assignElementId(event);
-			clickObject.ctrlKey = event.ctrlKey;
-			clickObject.altKey = event.altKey;
-			clickObject.target = event.target;
-			clickObject.timeStamp = event.timeStamp;
-			clickObject.x = event.x;
-			clickObject.y = event.y;
-			clickObject.path = event.path;
+			// clickObject.ctrlKey = event.ctrlKey;
+			// clickObject.altKey = event.altKey;
+			// clickObject.target = event.target;
+			// clickObject.timeStamp = event.timeStamp;
+			// clickObject.x = event.x;
+			// clickObject.y = event.y;
+			// clickObject.path = event.path;
 
 			console.dir(event);
 			console.dir(clickObject);
